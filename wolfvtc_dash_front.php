472d611f2bdddbb2566f7db3d639765c254e03fe
HumaneWolf <humanewolf@gmail.com>
2015-12-08 18:10:07 +0100

    small tweaks

--- wolfvtc_dash_front.php
+++ wolfvtc_dash_front.php
@@ -15,9 +15,6 @@ function wolfvtc_dash_front() {
 	if (get_option("wolfvtc_setup") == FALSE) {
 		echo '<div style="width:60%;margin-left:20%;margin-right:20%;background-color:#2980b9;text-align:center"><h1 style="color:#ffffff">Your admin has to do something!</h1></div>
 		<p>Please tell the site administrator to access the WolfVTC Admin Panel.</p>';
-
-		delete_option("wolfvtc_setup");
-		add_option("wolfvtc_setup", TRUE);
 	} elseif (!isset($_GET['do'])) {
 		if (wolfvtc_isuser(get_current_user_id())) {
 			echo '<a href="?page=wolfvtc&do=about"><input type="button" class="button-primary" style="width:100%" value="My profile"></a>
